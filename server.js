26fb4a4c7d153a6e7c05b1d97fe28b1d68f0c3ac
Drew <drewjw10@gmail.com>
2020-05-04 19:43:30 -0700

    First commit

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+// Connect Database
+connectDB();
+
+// Init Middleware
+
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API Running"));
+
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/artists", require("./routes/api/artists"));
+app.use("/api/performances", require("./routes/api/performances"));
+app.use("/api/songs", require("./routes/api/songs"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 const PORT = process.env.PORT || 5000;
 
